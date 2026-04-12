d6380c926f245ebc8c43589e439d35366b4b2e52
Devops-Developer909 <devopsdeveloper909@gmail.com>
2026-04-12 12:18:31 +0530

    UMapping update

--- src/main/java/com/cloud/product/controller/HelloController.java
+++ src/main/java/com/cloud/product/controller/HelloController.java
@@ -3,11 +3,18 @@ package com.cloud.product.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String hello() {
-        return "Hello from Azure 🚀";
+    public Map<String,String> getVmInfo() {
+        String hostName = System.getenv("HOST_NAME");
+        String hostIp = System.getenv("HOST_IP");
+        return Map.of(
+                "Hostname", hostName != null ? hostName : "Unknown",
+                "Host-IP", hostIp != null ? hostIp : "Unknown"
+        );
     }
 
 
